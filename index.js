aa468c686e2a5918a711274def8eb2e60724bb77
Ranjana R <rajagopalan.ranjana@gmail.com>
2025-08-15 01:11:43 -0500

    finished challenge 4

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ function addGamesToPage(games) {
 
         // append the game to the games-container
         gamesContainer.appendChild(div)
+    }
 }
 
 // call the function we just defined using the correct variable
@@ -66,19 +67,25 @@ addGamesToPage(GAMES_JSON);
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
-
+const totalContributions = GAMES_JSON.reduce( (acc, game) => acc + game.backers, 0)
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
+contributionsCard.innerHTML = `
+${totalContributions.toLocaleString('en-US')}
+`
 
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
+const amountRaised = GAMES_JSON.reduce( (acc, game) => acc + game.pledged, 0)
 
 // set inner HTML using template literal
+raisedCard.innerHTML = `$${amountRaised.toLocaleString('en-US')}`
 
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
+gamesCard.innerHTML = `${GAMES_JSON.length}`
 
 
 /*************************************************************************************
