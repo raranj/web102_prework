da95407e776d4ab1f37968d70c4d20e775a66af7
Ranjana R <rajagopalan.ranjana@gmail.com>
2025-08-15 02:07:37 -0500

    finished challenge 5

--- index.js
+++ index.js
@@ -99,28 +99,33 @@ function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have not yet met their goal
-
+    let filtered = GAMES_JSON.filter( (game) => {return game.goal > game.pledged});
+    // console.log(filtered.length);
 
     // use the function we previously created to add the unfunded games to the DOM
-
+    addGamesToPage(filtered);
 }
+
 
 // show only games that are fully funded
 function filterFundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have met or exceeded their goal
-
+    let filtered = GAMES_JSON.filter( (game) => game.goal <= game.pledged);
+    // console.log(filtered.length);
 
     // use the function we previously created to add unfunded games to the DOM
-
+    addGamesToPage(filtered)
 }
+
 
 // show all games
 function showAllGames() {
     deleteChildElements(gamesContainer);
 
     // add all games from the JSON data to the DOM
+    addGamesToPage(GAMES_JSON);
 
 }
 
@@ -130,6 +135,9 @@ const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
+unfundedBtn.addEventListener("click", filterUnfundedOnly);
+fundedBtn.addEventListener("click", filterFundedOnly);
+allBtn.addEventListener("click", showAllGames);
 
 
 /*************************************************************************************
